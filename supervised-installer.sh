b0fe2c101131eabd38daaf102157c2e71730b9b9
M0r87 <115916925+M0r87@users.noreply.github.com>
2023-03-01 23:37:50 +0100

    Update supervised-installer.sh

--- supervised-installer.sh
+++ supervised-installer.sh
@@ -271,9 +271,8 @@ systemctl start hassio-supervisor.service
 # Setup CLI
 info "Installing the 'ha' cli"
 info "Manually RUN: curl -sL $URL_HA > $PREFIX/bin/ha"
-curl -sL ${URL_HA} > "${PREFIX}/bin/ha"
+# curl -sL ${URL_HA} > "${PREFIX}/bin/ha"
 chmod a+x "${PREFIX}/bin/ha"
-
 info
 info "Home Assistant supervised is now installed"
 info "First setup will take some time, when it's ready you can reach it here:"
