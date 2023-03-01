c24d8b16b0c52c158e5af98ff372ceb80106b526
M0r87 <115916925+M0r87@users.noreply.github.com>
2023-03-01 22:52:31 +0100

    Update supervised-installer.sh

--- supervised-installer.sh
+++ supervised-installer.sh
@@ -226,9 +226,10 @@ info "Install supervisor startup scripts"
 info "Variabili"
 info "$URL_BIN_HASSIO"
 info "$URL_SERVICE_HASSIO"
+info "$PREFIX"
 curl -sL ${URL_BIN_HASSIO} > "${PREFIX}/sbin/hassio-supervisor"
 curl -sL ${URL_SERVICE_HASSIO} > "${SYSCONFDIR}/systemd/system/hassio-supervisor.service"
-
+info "Curl fatto"
 sed -i "s,%%HASSIO_CONFIG%%,${CONFIG},g" "${PREFIX}"/sbin/hassio-supervisor
 sed -i -e "s,%%BINARY_DOCKER%%,${BINARY_DOCKER},g" \
        -e "s,%%SERVICE_DOCKER%%,${SERVICE_DOCKER},g" \
